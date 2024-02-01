4532e471914b6f880c65e5be27e1b00ead30b5b6
changhs <changhs@glodon.com>
2024-02-01 11:16:47 +0800

    固定窗口计数器

--- algorithm/src/main/java/com/chs/ratelimit_chs/FixedWindowCounter.java
+++ algorithm/src/main/java/com/chs/ratelimit_chs/FixedWindowCounter.java
@@ -11,35 +11,46 @@ package com.chs.ratelimit_chs;
 public class FixedWindowCounter {
     private static int count = 0;
 
-    private static int qps = 2;
+    private static final int qps = 2;
 
-    //时间间隔,一分钟
-    private static int interval = 60 * 1000 * 1000;
+    /**
+     * 时间间隔,5秒
+     */
+    private static final int interval = 5 * 1000;
 
-    //当前时间
-    private static int startTime;
+    /**
+     * 前窗口的开始时间
+     */
+    private static long startTime;
 
     /**
      * 如果当前时间与开始时间的时间差大于时间间隔,则重置开始时间和计数器
      * 如果计数器小于qps,则计数器加1,否则拒绝访问
      */
-    public boolean rateLimit() {
+    public static boolean rateLimit() {
+        long now = System.currentTimeMillis();
+        if (now - startTime > interval) {
+            startTime = now;
+            count = 1;
+        } else {
+            if (count < qps) {
+                count++;
+            } else {
+                return false;
+            }
+        }
         return true;
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         for (int i = 0; i < 20; i++) {
-            FixedWindowCounter fixedWindowCounter = new FixedWindowCounter();
-            if (fixedWindowCounter.rateLimit()) {
+            Thread.sleep(500);
+            if (FixedWindowCounter.rateLimit()) {
                 System.out.println("正常访问");
             } else {
                 System.out.println("访问受限");
             }
         }
     }
-
-
-
-
 }
